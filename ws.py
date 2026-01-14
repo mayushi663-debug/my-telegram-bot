d03c3e22327f3a6d07ae53b862b22baaa2e4e492
mayushi663-debug <mayushi663@gmail.com>
2026-01-14 19:13:52 +0530

    Update ws.py

--- ws.py
+++ ws.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -5,10 +6,12 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is running 24/7!"
+    return "Bot is Online!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    # Render hamesha PORT mangta hai, ye line usey handle karegi
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
